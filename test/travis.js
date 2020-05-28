dd032e8f4a92e69f2f3b00aac1b17f0c37787623
Dominykas Blyžė <hello@dominykas.com>
2020-05-28 18:21:50 +0300

    feat: support indirect import

--- test/travis.js
+++ test/travis.js
@@ -33,33 +33,52 @@ describe('.travis.yml parsing', () => {
         fixture.cleanup();
     });
 
-    describe('simple', () => {
+    for (const yml of ['simple-single-object', 'simple-single-string', 'simple-array-object', 'simple-array-string']) {
 
-        for (const yml of ['simple-single-object', 'simple-single-string', 'simple-array-object', 'simple-array-string']) {
+        // eslint-disable-next-line no-loop-func
+        it(`resolves simple local import (${yml})`, async () => {
 
-            // eslint-disable-next-line no-loop-func
-            it(`resolves simple local import (${yml})`, async () => {
-
-                await fixture.setupRepoFolder({
-                    partials: true,
-                    travisYml: `testing-imports/${yml}.yml`
-                });
-
-                const result = await NodeSupport.detect({ path: fixture.path });
-
-                internals.assertCommit(result);
-
-                expect(result).to.equal({
-                    name: 'test-module',
-                    version: '0.0.0-development',
-                    timestamp: 1580673602000,
-                    travis: {
-                        raw: ['14'],
-                        resolved: { '14': '14.3.0' }
-                    }
-                });
+            await fixture.setupRepoFolder({
+                partials: true,
+                travisYml: `testing-imports/${yml}.yml`
             });
-        }
+
+            const result = await NodeSupport.detect({ path: fixture.path });
+
+            internals.assertCommit(result);
+
+            expect(result).to.equal({
+                name: 'test-module',
+                version: '0.0.0-development',
+                timestamp: 1580673602000,
+                travis: {
+                    raw: ['14'],
+                    resolved: { '14': '14.3.0' }
+                }
+            });
+        });
+    }
+
+    it('resolves indirect imports', async () => {
+
+        await fixture.setupRepoFolder({
+            partials: true,
+            travisYml: `testing-imports/indirect.yml`
+        });
+
+        const result = await NodeSupport.detect({ path: fixture.path });
+
+        internals.assertCommit(result);
+
+        expect(result).to.equal({
+            name: 'test-module',
+            version: '0.0.0-development',
+            timestamp: 1580673602000,
+            travis: {
+                raw: ['14'],
+                resolved: { '14': '14.3.0' }
+            }
+        });
     });
 
 });
